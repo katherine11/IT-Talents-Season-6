5a4d2f648b99e24995171bc24d50aee5052df51a
katherine11 <katherine.simeonova@gmail.com>
2016-12-29 15:56:04 +0200

    Removing immigrants task

--- immigrants/immigrants/interfaces/Migrateable.java
+++
@@ -1,9 +0,0 @@
-package immigrants.interfaces;
-import immigrants.other.City;
-
-public interface Migrateable {
-
-	void migrate(City city);
-	
-}
-
--- immigrants/immigrants/interfaces/Shootable.java
+++
@@ -1,9 +0,0 @@
-package immigrants.interfaces;
-import immigrants.other.City;
-import immigrants.weapons.Weapon;
-
-public interface Shootable {
-	
-	byte shoot(City city, Weapon weapon);
-
-}
